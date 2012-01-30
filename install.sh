483b6454a2dc39425cb906647c0d0137aab3ae9e
Jonas Brunsgaard <jonas.brunsgaard@gmail.com>
2012-01-30 17:48:35 +0100

    installer added

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DIR=`pwd`
+
+echo Installing vim config...
+ln -Tfs $DIR/{vimrc,vim} $HOME/{.vimrc,.vim}
+mkdir -p /var/tmp/vim/{swap,undo}
+
+echo Installing zsh config...
+ln -Tfs $DIR/zshrc $HOME/.zshrc
+
+echo Setting default shell to zsh...
+chsh -s `which zsh`
+
+echo Installing git config...
+ln -Tfs $DIR/gitignore_global $HOME/.gitignore_global
+
+echo All done!
