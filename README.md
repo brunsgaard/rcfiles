3bbaa593106c52ae9d5b3a606199e017f449f702
Jonas Brunsgaard <jonas.brunsgaard@gmail.com>
2012-01-30 20:50:51 +0100

    update

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 Thanks goes to [vicfryzel](https://github.com/vicfryzel)  for sharing his xmonad configs.
 
-Also some configs i have from my good freind [Eckankar](https://github.com/Eckankar)
+Also my good freind [Eckankar](https://github.com/Eckankar) has been an excelent source of config files etc.:
 
 
--- install.sh
+++ install.sh
@@ -2,10 +2,21 @@
 
 DIR=`pwd`
 
+echo Installing xinitrc and Xresources
+ln -Tfs $DIR/xinitrc $HOME/.xinitrc
+ln -Tfs $DIR/Xresources $HOME/.Xresources
+
 echo Installing vim config...
 ln -Tfs $DIR/vimrc $HOME/.vimrc
 ln -Tfs $DIR/vim $HOME/.vim
 mkdir -p /var/tmp/vim/{swap,undo}
+
+echo Installing zsh config...
+ln -Tfs $DIR/zshrc $HOME/.zshrc
+
+echo Installing xmonad and xmobar config...
+ln -Tfs $DIR/xmobar.hs $HOME/.xmonad/xmobar.hs
+ln -Tfs $DIR/xmonad.hs $HOME/.xmonad/xmonad.hs
 
 echo Installing zsh config...
 ln -Tfs $DIR/zshrc $HOME/.zshrc
